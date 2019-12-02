37c3cad672bb097b27d8de52772678666879dfab
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-12-02 13:47:47 -0800

    Move generic helpers to all_targets
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

---
+++ all_targets.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""
+These constants and functions are useful for all target types
+"""
+
+import itertools
+
+PORT_PROTOCOLS = [
+    "ahb",
+    "apb",
+    "axi4",
+    "tl",
+]
+
+PORT_TYPES = [
+    "periph",
+    "sys",
+    "mem",
+]
+
+# The resulting order of the contents of PORTS is sensitive and should not be modified without
+# first consulting the RTL testbench developers or RTL simulation will break.
+PORTS = ["sifive,%s-%s-port" % (protocol, port_type) \
+             for protocol, port_type in itertools.product(PORT_PROTOCOLS, PORT_TYPES)]
+
+CAP_SIZE_FOR_VCS = 0x1fffffff
+
+def get_boot_hart(tree):
+    """Given a tree, return the node which should be used as the boot hart"""
+    riscv_harts = tree.match("^riscv$")
+    for hart in riscv_harts:
+        if hart.get_reg()[0][0] == 1:
+            return hart
+    return riscv_harts[0]
+
+def number_to_cells(num, num_cells):
+    """Convert an integer into 32-bit cells"""
+    cells = []
+    for i in range(num_cells):
+        cells.insert(0, (0xFFFFFFFF & (num >> (32 * i))))
+    return " ".join(["0x%x" % x for x in cells])
--- testbench.py
+++ testbench.py
@@ -5,45 +5,11 @@ This is a python script for generating RTL testbench Devicetree overlays from th
 for the RTL DUT.
 """
 
-import itertools
 import sys
 
 import pydevicetree
 
-PORT_PROTOCOLS = [
-    "ahb",
-    "apb",
-    "axi4",
-    "tl",
-]
-
-PORT_TYPES = [
-    "periph",
-    "sys",
-    "mem",
-]
-
-# The resulting order of the contents of PORTS is sensitive and should not be modified without
-# first consulting the RTL testbench developers or RTL simulation will break.
-PORTS = ["sifive,%s-%s-port" % (protocol, port_type) \
-             for protocol, port_type in itertools.product(PORT_PROTOCOLS, PORT_TYPES)]
-
-CAP_SIZE_FOR_VCS = 0x1fffffff
-
-def get_boot_hart(tree):
-    """Given a tree, return the node which should be used as the boot hart"""
-    riscv_harts = tree.match("^riscv$")
-    for hart in riscv_harts:
-        if hart.get_reg()[0][0] == 1:
-            return hart
-    return riscv_harts[0]
-
-def number_to_cells(num, num_cells):
-    """Convert an integer into 32-bit cells"""
-    cells = []
-    for i in range(num_cells):
-        cells.insert(0, (0xFFFFFFFF & (num >> (32 * i))))
-    return " ".join(["0x%x" % x for x in cells])
+from all_targets import PORTS, CAP_SIZE_FOR_VCS, get_boot_hart, number_to_cells
 
 def attach_testrams(tree, overlay):
     """Generate testrams attached to ports in the overlay
