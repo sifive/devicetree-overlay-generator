33d436a527b7a613232b20c99b641552a70ef1ce
Nathaniel Graff <nathaniel.graff@sifive.com>
2020-01-14 13:43:55 -0800

    Fix QEMU overlays
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- targets/qemu.py
+++ targets/qemu.py
@@ -7,16 +7,20 @@ This is a python script for generating RTL testbench Devicetree overlays from th
 for the RTL DUT.
 """
 
-from targets.generic import set_boot_hart, set_stdout, set_entry, get_spi_flash, get_rams, set_rams
+from targets.generic import set_boot_hart, set_stdout, set_entry, get_spi_flash, get_spi_region, get_rams, set_rams
 
 def generate_overlay(tree, overlay):
     """Generate the overlay"""
     spi = get_spi_flash(tree)
     memory = tree.get_by_path("/memory")
     if spi is not None:
-        set_entry(overlay, spi, 0, 0x400000)
+        region = get_spi_region(spi)
+        set_entry(overlay, spi, region, 0x400000)
     else:
         set_entry(overlay, memory, 0, 0x0)
 
     set_boot_hart(tree, overlay)
     set_stdout(tree, overlay, 115200)
+
+    ram, itim = get_rams(tree)
+    set_rams(overlay, ram, itim)
