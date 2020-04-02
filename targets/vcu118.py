54345a0cddc18b59e13d5e080ec0cae849761f0e
Nathaniel Graff <nathaniel.graff@sifive.com>
2020-04-02 12:49:00 -0700

    Use vc707 strategy on vcu118
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- targets/vcu118.py
+++ targets/vcu118.py
@@ -3,21 +3,27 @@
 # SPDX-License-Identifier: Apache-2.0
 
 """
-This is a python script for generating RTL testbench Devicetree overlays from the Devicetree
-for the VCU118 DUT.
+This is a python script for generating VCU118 Devicetree overlays from the Devicetree
+for the core on the VCU118.
 """
 
-from targets.generic import set_boot_hart, set_stdout, set_entry, get_spi_flash, get_spi_region, get_rams, set_rams
+from targets.generic import set_boot_hart, set_stdout, set_entry, get_spi_flash
+from targets.generic import get_spi_region, get_rams, set_rams, get_dtim, get_boot_hart
 
 def generate_overlay(tree, overlay):
     """Generate the overlay"""
-    bootrom = get_spi_flash(tree)
+    bootrom = tree.get_by_path("/memory")
     if bootrom is not None:
-        region = get_spi_region(bootrom)
-        set_entry(overlay, bootrom, region, 0x400000)
+        set_entry(overlay, bootrom, 0, 0)
 
     set_boot_hart(tree, overlay)
     set_stdout(tree, overlay, 115200)
 
     ram, itim = get_rams(tree)
+
+    # Prioritize the dtim over /memory
+    dtim = get_dtim(tree, get_boot_hart(tree))
+    if dtim is not None:
+        ram = dtim
+
     set_rams(overlay, ram, itim)
